dfcdf20acb2ea20dbaf20faa45c4229b69568a88
hasinilakshika <hasinilakshika56@gmail.com>
2025-12-28 23:10:35 +0530

    Fix vite config duplicates

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from "path"
-
-// https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react(), tailwindcss()],
-//   resolve: {
-//     alias: {
-//       "@": path.resolve( __dirname, "./src"),
-
-//     },
-//   },
-// });
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/", // or just remove base (default is "/")
+  base: "/",
   plugins: [react()],
 });
