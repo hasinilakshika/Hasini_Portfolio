351339de925789fab2e9286157ac1e64a7998795
hasinilakshika <hasinilakshika56@gmail.com>
2025-12-29 02:12:28 +0530

    test

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
-  base: "/Hasini_Portfolio",
+  base: "/Hasini_Portfoli",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
